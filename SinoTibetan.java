d1095351828ad22f8c947c16f26bb8420afffba4
Adam Hakimi <111243011+aadaamn@users.noreply.github.com>
2023-03-10 01:23:07 +0800

    Delete SinoTibetan.java

--- SinoTibetan.java
+++
@@ -1,9 +0,0 @@
-public class SinoTibetan extends Language {
-    SinoTibetan(String languageName, int speakers) {
-    super(languageName, speakers, "Asia","subject-object-verb");
-    if (this.name.contains("Chinese")) 
-      this.wordOrder = "subject-verb-object";
-  }
-
-    
-  }
